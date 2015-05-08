5660dbbfd93eb05fec6f8876c50eda85c6cc41a3
aidad <aida.delfan@gmail.com>
2015-05-07 20:19:14 -0400

    modified get_tweets_vocab

--- TweetToMongo.py
+++ TweetToMongo.py
@@ -23,10 +23,10 @@ class TweetToMongo:
         try:
             self.client = pymongo.Connection(host = 'localhost', port = 27017)
             self.database = self.client[db_name]
-            print 'Connected successfully'
+            print 'Connected successfully \n'
         except:
             #in case of errors
-            print 'Could not connect to MongoDB'
+            print 'Could not connect to MongoDB \n'
 
 
 
@@ -61,27 +61,27 @@ class TweetToMongo:
 
 
 
-    def get_tweets_vocab(self, vocab, seed_vocab, collection_name):
+    def get_tweets_vocab(self, vocab, collection_name):
         #take a seed vocab and build a new vocab of tweets containing
         #words of the seed vocab
         db = self.database
-        num_features = 2000
-        #initialize bag of words object
-        cv = CountVectorizer(analyzer = 'word', max_features = num_features)
 
         parsed_tweets = []
 
         regex_string = '|'.join(vocab)
         REGEX = re.compile(regex_string)
 
-        print 'Filtering tweets containing a word of the vocab ...\n'
+        print 'Filtering tweets containing a word of the vocab ... \n'
         filtered_tweets = db[collection_name].find({'text': {'$regex':REGEX}})
 
         num = 1
         for tweet in filtered_tweets:
 
-            if num>=5000 and num%5000 == 0:
+            if num>=10000 and num%10000 == 0:
                 print 'Parsing filtered tweet %d  \n' %num
+
+            # could use len(re.findall(regex_string, tweet['text'])) to see how
+            # many matches are in each tweet
 
             #use the tweet_to_words method to parse tweets
             parsed_tweet = TweetParsing.tweet_to_words(tweet['text'],
@@ -90,24 +90,31 @@ class TweetToMongo:
             parsed_tweets.append(parsed_tweet)
             num += 1
 
-        print 'Bag of words ...\n'
-        #find a vocab of the filtered tweets and vectorize them using a bag of word
-        #model
+        print 'Bag of words ... \n'
+        #find a vocab of the filtered tweets and vectorize them using bag of words
+
+        # vocabulary to be specified in the CountVectorizer, should be a dic
+        # vocab_dic = {}
+        # for item in vocab:
+        #     vocab_dic[item] = vocab.index(item)
+
+        num_features = 2000
+
+        # initialize bag of words object
+        cv = CountVectorizer(analyzer = 'word', max_features = num_features)
+
         vectorized_tweets = cv.fit_transform(parsed_tweets).toarray()
 
         #get words with their frequency in a list of tuples
-        words_freq = zip(cv.get_feature_names(), vectorized_tweets.sum(axis=0))
+        words_freq = zip(cv.get_feature_names(), vectorized_tweets.sum(axis = 0))
 
-        #sort the list by frequency of words
+        #sort the list by frequency of words, ascending
         words_freq = sorted(words_freq, key = lambda x: x[1])
 
         new_vocab = [item[0] for item in words_freq]
 
         #select only those elements that are not too frequent (to avoid words
-        #like 'today' that are not useful)
-        new_vocab = new_vocab[len(new_vocab)//10:2*len(new_vocab)//10]
+        #like 'today' that are not useful) or too rare
+        new_vocab = new_vocab[len(new_vocab)//5:4*len(new_vocab)//5]
 
-        #concatenate selected elements of the new vocab to the seed vocab
-        vocab = set(seed_vocab + new_vocab)
-
-        return vocab
+        return new_vocab
